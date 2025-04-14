29a6a6b4eedc552c937545ac2d27d95a6d228dcb
jessica.paiva <jessicapaivamvp@gmail.com>
2025-04-14 20:17:47 -0300

    TrabalhoJogo code

---
+++ code/player.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Interface2 import Interface2
+
+
+class Player(Interface2):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
