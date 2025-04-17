614a46ed2fa1d39d01ceaa8848d9a85b38e9986a
Junior <juniorpires.stim@gmail.com>
2025-04-16 22:18:41 -0300

    TrabalhoJogo code

--- code/Game.py
+++ code/Game.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGTH
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGTH))
 
     def run(self):
         while True:
+            pygame.mixer_music.load('asset/musica.mp3')
+            pygame.mixer_music.play(-1)
             menu = Menu(self.window)
             menu.run()
             pass
--- code/Menu.py
+++ code/Menu.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+
 
 class Menu:
     def __init__(self,window):
         self.window = window
+        self.surf = pygame.image.load('asset/PNG/War2/Bright/War2.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
+
     def run(self, ):
+        self.window.blit(self.surf, self.rect)
+        pygame.display.flip()
         pass
