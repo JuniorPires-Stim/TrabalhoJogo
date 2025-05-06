0cc74cfdae3588f8c337b4e39048921261b2de71
Junior <juniorpires.stim@gmail.com>
2025-05-06 00:46:52 -0300

    resolvendo o problema do level1

--- code/level.py
+++ code/level.py
@@ -11,12 +11,12 @@ class Level:
         self.window = window
         self.name = name
         self.game_mode = game_mode
-        self.entity_lis: list[Entity] = []
+        self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
     def run(self, ):
         while True:
-            for ent in self.entity_lis:
+            for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 pygame.display.flip()
             pass
