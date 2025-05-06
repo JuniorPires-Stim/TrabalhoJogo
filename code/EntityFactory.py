67dd4089802ad9459f4430d0a25f6a5b13939ceb
Junior <juniorpires.stim@gmail.com>
2025-05-05 23:14:03 -0300

    resolvendo o problema do level1

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,12 +1,12 @@
 from code.Background import Background
-from code.Entity import Entity
+
 
 class EntityFactory:
     @staticmethod
-    def get_entity(self, entity_name: str, position=(0,0)):
+    def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
                 for i in range(11):
-                    list_bg.append(Background(name=f'Level1Bg{i}', position=(0, 0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                 return list_bg
