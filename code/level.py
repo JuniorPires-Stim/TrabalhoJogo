a189458c8166e10eba289a278aef7301b7c54987
Junior <juniorpires.stim@gmail.com>
2025-05-05 23:15:43 -0300

    resolvendo o problema do level1

--- code/level.py
+++ code/level.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import pygame.display
-from code import Entity
+import pygame
+
+from code.Entity import Entity
+from code.EntityFactory import EntityFactory
 
 
 class Level:
@@ -10,7 +12,7 @@ class Level:
         self.name = name
         self.game_mode = game_mode
         self.entity_lis: list[Entity] = []
-        #self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
     def run(self, ):
         while True:
