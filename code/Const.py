4aee3bbdbc254bcebfcff99aefaa0f14d31ba413
Junior <juniorpires.stim@gmail.com>
2025-05-06 21:27:52 -0300

    ajustando velocidade das imagens

--- code/Const.py
+++ code/Const.py
@@ -13,3 +13,16 @@ MENU_OPTION =("NEW GAME 1P",
               "NEW GAME 2P COMPETITIVE",
               "SCORE",
               "EXIT")
+
+#E
+ENTITY_SPEED = {'Level1Bg0': 0,
+                'Level1Bg1': 0,
+                'Level1Bg2': 1,
+                'Level1Bg3': 4,
+                'Level1Bg4': 7,
+                'Level1Bg5': 10,
+                'Level1Bg6': 13,
+                'Level1Bg7': 16,
+                'Level1Bg8': 19,
+                'Level1Bg9': 22,
+                'Level1Bg10': 25,}
